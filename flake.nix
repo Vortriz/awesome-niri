ba7d99f29f7908871a8481864930dbec6f66776d
Vortriz <vorarishi22+github@gmail.com>
2025-12-13 16:53:15 +0530

    flake: change formatter

--- flake.nix
+++ flake.nix
@@ -1,35 +1,60 @@
 {
-  description = "A simple env for awesome-niri";
+    description = "A simple env for awesome-niri";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    inherit (nixpkgs) lib legacyPackages;
+    outputs =
+        { nixpkgs, ... }:
+        let
+            inherit (nixpkgs) lib legacyPackages;
 
-    systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-    forAllSystems = f: (lib.genAttrs systems) (system: f legacyPackages.${system});
-  in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
+            systems = [
+                "aarch64-darwin"
+                "aarch64-linux"
+                "x86_64-darwin"
+                "x86_64-linux"
+            ];
+            forAllSystems = f: (lib.genAttrs systems) (system: f legacyPackages.${system});
+        in
+        {
+            formatter = forAllSystems (
+                pkgs:
+                pkgs.treefmt.withConfig {
+                    runtimeInputs = with pkgs; [
+                        nixfmt
+                        prettier
+                    ];
+                    settings.formatter = {
+                        nixfmt = {
+                            command = "nixfmt";
+                            includes = [ "*.nix" ];
+                            options = [ "--indent=4" ];
+                        };
+                        prettier = {
+                            command = "prettier";
+                            includes = [ "*.md" ];
+                            options = [
+                                "--tab-width"
+                                "4"
+                            ];
+                        };
+                    };
+                }
+            );
 
-    packages = forAllSystems (pkgs: {lint = pkgs.nodePackages.awesome-lint;});
+            packages = forAllSystems (pkgs: {
+                lint = pkgs.nodePackages.awesome-lint;
+            });
 
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShell {
-        packages = [
-          pkgs.git
-        ];
-
-        shellHook = ''
-          echo "Pulling latest changes..."
-          git pull
-        '';
-      };
-    });
-  };
+            devShells = forAllSystems (pkgs: {
+                default = pkgs.mkShell {
+                    shellHook = ''
+                        echo "Pulling latest changes..."
+                        git pull
+                    '';
+                };
+            });
+        };
 }
